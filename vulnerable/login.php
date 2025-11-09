e171ead264c89bb894c913ff9dce6b72a4d4e2c7
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-11-09 23:57:20 +0530

    create vulnerable login page

---
+++ vulnerable/login.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head><title>CSRF_vulnerability</title>
+</head>
+<body>
+
+    <h2>Login Form</h2>
+    <form>
+        <input type="email" name="email" placeholder="Email" required><br><br>
+        <input type="password" name="password" placeholder="Password" required><br><br>
+        <button type="submit">Login</button>
+    </form>
+    
+</body>
+</html>
