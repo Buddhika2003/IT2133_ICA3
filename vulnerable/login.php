e96b7ffba918730ecba10ed4332507df7ae72d31
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-11-10 00:04:29 +0530

    add server side login validation and error message

--- vulnerable/login.php
+++ vulnerable/login.php
@@ -1,3 +1,24 @@
+<?php
+    session_start();
+
+    $err='';
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        $email = $_POST['email'];
+        $password = $_POST['password'];
+
+        $correct_email = "UOV_2025@gmail.com";
+        $correct_password = "UOV2025";
+
+        if($email == $correct_email && $password == $correct_password){
+            $_SESSION['log_in'] = true;
+            $_SESSION['user_email'] = $email;
+            header("Location: index.php");
+            exit;
+        } else {
+            $err = "Invalid email or password.";
+        }
+    }
+?>
 <!DOCTYPE html>
 <html>
 <head><title>CSRF_vulnerability</title>
