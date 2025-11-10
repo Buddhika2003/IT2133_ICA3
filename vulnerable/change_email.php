aae4a9bde1de1f13a921f73ff722860111c7de36
Budddhika Rathnayake <rathnayakearmb@gmail.com>
2025-11-10 23:53:20 +0530

    Update change email form to include validation and success message

--- vulnerable/change_email.php
+++ vulnerable/change_email.php
@@ -3,6 +3,16 @@
         header('Location:ogin.php');
         exit;
     }
+
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
+       $new = filter_var($_POST['new_email']?? '', FILTER_VALIDATE_EMAIL);
+    if($new){
+        $msg ="Invalid email";
+    }else{
+        $_SESSION['email'] = $new;
+        $msg = "Email changed successfully". htmlspecialchars($new) ;
+    }
+}
 ?>
 
 
@@ -16,6 +26,10 @@
         <input type="email" name="new_email" placeholder="New Email" required>
         <button type="submit" value="Change Email"></button>
     </form>
-    
+    <?php if (!empty($msg)): ?>
+        <p style="color:green;"><?php echo $msg; ?></p>
+    <?php endif; ?>
+
+    <p><a href="index.php">Back to Dashboard</a></
 </body>
 </html>
